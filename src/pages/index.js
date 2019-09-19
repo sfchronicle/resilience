c197d396d8258aa8ddff7e26fcc006e4ba0a54dc
ewagstaff <evanjwagstaff@gmail.com>
2019-09-18 17:32:12 -0700

    More mobile fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,11 +38,18 @@ export default class IndexPage extends PureComponent {
   }
 
   componentDidMount() {
-    this.timeout = setTimeout(() => {
+    // Only delay map layer on desktop
+    if (window.innerWidth > 979){
+      this.timeout = setTimeout(() => {
+        this.setState({
+          layer: "fires"
+        })
+      }, 3000)
+    } else {
       this.setState({
         layer: "fires"
-      })
-    }, 3000)
+      });
+    }
   }
 
   setLayer(type){
@@ -172,7 +179,7 @@ export default class IndexPage extends PureComponent {
 
   							<p className="instructions">This map highlights the risk areas in California for three major natural disasters: earthquakes, fires and floods. Select a layer to see the risk areas.</p>
 
-                <div className="layer-text">
+                <div className="layer-text desktop">
                   <p className={this.state.layer === "fires" ? "instructions show" : "instructions"}>Climate change will bring more and more wildfires through residential areas. The best way to fight them begins before your neighborhood is at risk.<br /><br /><a href="" target="_blank">Lessons from Santa Rosa &raquo;</a></p>
                   <p className={this.state.layer === "quakes" ? "instructions show" : "instructions"}>There's a 72% probability that a major earthquake will strike the Bay Area by 2043. Here's what your community should do right now.<br /><br /><a href="" target="_blank">Lessons from San Francisco &raquo;</a></p>
                   <p className={this.state.layer === "floods" ? "instructions show" : "instructions"}>Tens of thousands of Bay Area homes may experience chronic flooding as sea levels rise. What one city learned when it happened to them in 2017.<br /><br /><a href="" target="_blank">Lessons from San Jose &raquo;</a></p>
@@ -188,6 +195,12 @@ export default class IndexPage extends PureComponent {
           </div>
 
           <div className="article">
+            <div className="layer-text mobile">
+              <p className={this.state.layer === "fires" ? "instructions show" : "instructions"}>Climate change will bring more and more wildfires through residential areas. The best way to fight them begins before your neighborhood is at risk.<br /><a href="" target="_blank">Lessons from Santa Rosa &raquo;</a></p>
+              <p className={this.state.layer === "quakes" ? "instructions show" : "instructions"}>There's a 72% probability that a major earthquake will strike the Bay Area by 2043. Here's what your community should do right now.<br /><a href="" target="_blank">Lessons from San Francisco &raquo;</a></p>
+              <p className={this.state.layer === "floods" ? "instructions show" : "instructions"}>Tens of thousands of Bay Area homes may experience chronic flooding as sea levels rise. What one city learned when it happened to them in 2017.<br /><a href="" target="_blank">Lessons from San Jose &raquo;</a></p>
+            </div>
+
             {docData.mainbar.map((item) => {
               if (item.type === "text"){
                 return <p dangerouslySetInnerHTML={{__html: item.value}}></p>
